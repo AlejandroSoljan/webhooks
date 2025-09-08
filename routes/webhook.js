c582c2000225ffd2f09f4156cc53e84cb03593af
AlejandroSoljan <116036946+AlejandroSoljan@users.noreply.github.com>
2025-09-08 16:27:07 -0300

    Delete routes directory

--- routes/webhook.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { handleWebhookGet, handleWebhookPost } = require('../services/webhookService');
-const router = express.Router();
-router.get('/webhook', handleWebhookGet);
-router.post('/webhook', handleWebhookPost);
-module.exports = router;
