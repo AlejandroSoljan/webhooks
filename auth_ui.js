30a022a2dbb9ee060963b9a5142c6570f2348fd6
AlejandroSoljan <116036946+AlejandroSoljan@users.noreply.github.com>
2026-01-15 18:09:25 -0300

    Add files via upload

--- auth_ui.js
+++ auth_ui.js
@@ -2176,18 +2176,15 @@ function mountAuthRoutes(app) {
           return s;
         }
 
-        function addRow(key='', value=''){
+           function addRow(key='', value=''){
           const tr = document.createElement('tr');
-          // OJO: no usar backticks acá porque este JS vive dentro de un template literal del backend.
-          // Si quedan backticks anidados, el archivo puede romperse al compilar (Unexpected token '<').
           tr.innerHTML =
-            '<td><input class="inp" data-k value="' + esc(key) + '" placeholder="campo"/></td>' +
-            '<td><input class="inp" data-v value="' + esc(value) + '" placeholder="valor"/></td>' +
+            '<td><input class="inp" data-k value=\"' + esc(key) + '\" placeholder=\"campo\"/></td>' +
+            '<td><input class="inp" data-v value=\"' + esc(value) + '\" placeholder=\"valor\"/></td>' +
             '<td><button class="btn2" type="button" data-rm>✕</button></td>';
           tr.querySelector('[data-rm]').addEventListener('click', ()=> tr.remove());
           fieldsEl.appendChild(tr);
         }
-
 
         function setFieldsFromDoc(doc){
           fieldsEl.innerHTML = '';
