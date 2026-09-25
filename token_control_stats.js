ed739dafb9427a40ba6c3abf5a933e5a4953ed93
alejandrosoljan <alejandrosoljan@hotmail.com>
2026-09-25 18:19:20 -0300

    chore: publicar recalculo como v5.00.241

--- token_control_stats.js
+++ token_control_stats.js
@@ -1,4 +1,4 @@
-// Asisto | Version: 5.00.238 | Fecha: 2026-09-25
+// Asisto | Version: 5.00.241 | Fecha: 2026-09-25
 // token_control_stats.js
 // Panel y API para control de tokens por dominio, conversación y pedido completado.
  
